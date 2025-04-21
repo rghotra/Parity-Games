252a6e0f076feaafd05af41fe0a94f97b4a3aa1d
rghotra <ghotrarohan@gmail.com>
2025-04-20 22:26:36 -0400

    started randomizing pcp zetamac

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { useState } from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Home'
+import PCPZetamac2 from './PCPZetamac2'
 
 function App() {
 
   return (
-    <div className="d-flex justify-content-center align-items-start">
-      <Home />
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/pcp_zetamac_2' element={<PCPZetamac2 />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,36 +1,38 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
-  const [duration, setDuration] = useState('120');
 
   return (
-    <div className='p-4' style={{ maxWidth: 500, backgroundColor: '#DDDDDD' }}>
+    <div className="d-flex justify-content-center align-items-start">
+      <div className='p-4' style={{ maxWidth: 500, backgroundColor: '#DDDDDD' }}>
 
-      <h1 className="mb-4">Parity Games</h1>
-      <p>
-        Games to build the market making mindset.
-      </p>
+        <h1 className="mb-4">Parity Games</h1>
+        <p>
+          Mr. Miyagi-ing a market maker.
+        </p>
 
-      <ol>
+        <ol>
 
-        <li> <a target="_blank" rel="noopener noreferrer" href='https://arithmetic.zetamac.com/game?key=a7220a92'> Vanilla Zetamac </a> </li>
+          <li> <a target="_blank" rel="noopener noreferrer" href='https://arithmetic.zetamac.com/game?key=a7220a92'> Vanilla Zetamac </a> </li>
 
-        <li> <a target="_blank" rel="noopener noreferrer" href='https://arithmetic.zetamac.com/game?key=d5094dbc'> 3-digit Add/Subtract Zetamac </a> </li>
+          <li> <a target="_blank" rel="noopener noreferrer" href='https://arithmetic.zetamac.com/game?key=d5094dbc'> 3-digit Add/Subtract Zetamac </a> </li>
 
-        <li> <a target="_blank" rel="noopener noreferrer" href='https://parity-zetamac.herokuapp.com/'> PCP Zetamac </a> </li>
+          <li> <a target="_blank" rel="noopener noreferrer" href='https://parity-zetamac.herokuapp.com/'> PCP Zetamac </a> </li>
 
-        <li> <a target="_blank" rel="noopener noreferrer" href='#'> PCP Zetamac 2.0 </a> </li>
+          <li> <Link to='/pcp_zetamac_2'> PCP Zetamac 2.0 </Link> </li>
 
-        <li> <a target="_blank" rel="noopener noreferrer" href='#'> Arbitrage Zetamac </a> </li>
+          <li> <a target="_blank" rel="noopener noreferrer" href='#'> Arbitrage Zetamac </a> </li>
 
-        <li> <a target="_blank" rel="noopener noreferrer" href='#'> Kenstonks </a> </li>
+          <li> <a target="_blank" rel="noopener noreferrer" href='#'> Kenstonks </a> </li>
 
-        <li> <a target="_blank" rel="noopener noreferrer" href='#'> Options Board Arbitrage Sim </a> </li>
+          <li> <a target="_blank" rel="noopener noreferrer" href='#'> Options Board Arbitrage Sim </a> </li>
 
-        <li> <a target="_blank" rel="noopener noreferrer" href='#'> Mock Trading </a> </li>
+          <li> <a target="_blank" rel="noopener noreferrer" href='#'> Mock Trading </a> </li>
 
-      </ol>
+        </ol>
 
+      </div>
     </div>
   );
 }
